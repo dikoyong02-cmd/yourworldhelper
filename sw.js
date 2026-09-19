662b417ef0a278d8189e85cf3362de1d974c3094
dikoyong02-cmd <dikoyong02@gmail.com>
2026-09-20 03:07:53 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "world-helper-v1";
+const CACHE = "world-helper-v2";
 const SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
